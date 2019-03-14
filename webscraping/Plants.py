eb7a8dd2db00fe09681b691324c73fca55145a27
ErickMachado95 <machado.m.erick@gmail.com>
2019-03-13 20:31:46 -0500

    made a class for plant so that we can datascrape and save information a lot easier

---
+++ webscraping/Plants.py
@@ -0,0 +1,15 @@
+import selenium
+
+
+
+class Plants:
+
+    def __init__(self,Sciname):
+        self.SciName = Sciname
+        self.Lat = Lat
+        self.Long = Long
+        self.Econregion = Eco
+        self.Statepark = State
+
+    def geteco(self):
+        print("in progress")
