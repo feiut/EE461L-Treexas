fd04f551e1133912432e09f1e04bfea264cfa7cf
ErickMachado95 <machado.m.erick@gmail.com>
2019-03-25 19:54:12 -0500

    fixed model.py to include other fields

--- Application/plant_app/plants/plantsite/models.py
+++ Application/plant_app/plants/plantsite/models.py
@@ -17,7 +17,10 @@ class EcoRegion(models.Model):
 
 #Hold the data and fields for plants.
 class PlantCsv(models.Model):
- #   dbid = models.IntegerField(blank=True, null=True)
+ #  dbid = models.IntegerField(blank=True, null=True)
+    native = models.TextField(db_column='Native', blank=True, null=True)  # Field name made lowercase.
+    image = models.TextField(db_column='Image', blank=True, null=True)  # Field name made lowercase.
+    name = models.TextField(db_column='Name', blank=True, null=True)  # Field name made lowercase.
     alsoknownas = models.TextField(db_column='AlsoKnownAs', blank=True, null=True)  # Field name made lowercase.
     botanicalname = models.TextField(db_column='BotanicalName', blank=True, null=True)  # Field name made lowercase.
     planttype = models.TextField(db_column='PlantType', blank=True, null=True)  # Field name made lowercase.
@@ -25,10 +28,10 @@ class PlantCsv(models.Model):
     waterdemand = models.TextField(db_column='WaterDemand', blank=True, null=True)  # Field name made lowercase.
     landscapeuse = models.TextField(db_column='LandscapeUse', blank=True, null=True)  # Field name made lowercase.
     ornamentalvalue = models.TextField(db_column='OrnamentalValue', blank=True, null=True)  # Field name made lowercase.
-    nativeadapted = models.TextField(db_column='NativeAdapted', blank=True, null=True)  # Field name made lowercase.
-    wildlifevalue = models.TextField(db_column='WildlifeValue', blank=True, null=True)  # Field name made lowercase.
+    native_adapted = models.TextField(db_column='Native/Adapted', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+    wildlife_alue = models.TextField(db_column='Wildlife alue', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
     season = models.TextField(db_column='Season', blank=True, null=True)  # Field name made lowercase.
-    deciduousevergreen = models.TextField(db_column='DeciduousEvergreen', blank=True, null=True)  # Field name made lowercase.
+    deciduous_evergreen = models.TextField(db_column='Deciduous/Evergreen', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
     plantform = models.TextField(db_column='PlantForm', blank=True, null=True)  # Field name made lowercase.
     plantspread = models.TextField(db_column='PlantSpread', blank=True, null=True)  # Field name made lowercase.
     plantheight = models.TextField(db_column='PlantHeight', blank=True, null=True)  # Field name made lowercase.
