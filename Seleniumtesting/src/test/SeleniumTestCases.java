f1ebd5c3737a14dc8cb31aa7fde3adaf39d0ed94
ec505 <ericcen999@gmail.com>
2019-03-30 21:57:47 -0500

    selenium testing for phase 2

--- Seleniumtesting/src/test/SeleniumTestCases.java
+++ Seleniumtesting/src/test/SeleniumTestCases.java
@@ -2,6 +2,7 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -15,6 +16,7 @@ import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SeleniumTestCases {
 	private static WebDriver driver;
@@ -23,45 +25,69 @@ public class SeleniumTestCases {
 		System.setProperty("webdriver.chrome.driver", "./lib/chromedriver/chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		WebDriverWait wait = new WebDriverWait(driver, 10);
 	}
 
 	@Test
 	public void mainPage() {//test 3 working links
 		driver.get("http://www.treexas.com/");
-		driver.findElement(By.xpath("//a[@id=\"plantlist\"]")).click();
-		assertEquals(driver.getCurrentUrl(),"http://www.treexas.com/plant_list/");
+		try {
+			TimeUnit.SECONDS.sleep(10);
+		} catch (InterruptedException e2) {
+			e2.printStackTrace();
+		}
+		driver.findElement(By.xpath("/html/body/nav/div/div[2]/ul/li[1]/a")).click();
+		assertEquals("http://www.treexas.com/plant_list/",driver.getCurrentUrl());
 		driver.navigate().back();
 		assertEquals(driver.getCurrentUrl(),"http://www.treexas.com/");
-		driver.findElement(By.xpath("//a[@id=\"ecoregions\"]")).click();
-		assertEquals(driver.getCurrentUrl(),"http://www.treexas.com/eco_list/");
+		driver.findElement(By.xpath("/html/body/nav/div/div[2]/ul/li[3]/a")).click();
+		assertEquals("http://www.treexas.com/eco_list/",driver.getCurrentUrl());
 		driver.navigate().back();
-		assertEquals(driver.getCurrentUrl(),"http://www.treexas.com/");
-		driver.findElement(By.xpath("//a[@id=\"stateparks\"]")).click();
-		assertEquals(driver.getCurrentUrl(),"http://www.treexas.com/park_list/");
+		assertEquals("http://www.treexas.com/",driver.getCurrentUrl());
+		driver.findElement(By.xpath("/html/body/nav/div/div[2]/ul/li[2]/a")).click();
+		assertEquals("http://www.treexas.com/park_list/",driver.getCurrentUrl());
 		driver.navigate().back();
-		assertEquals(driver.getCurrentUrl(),"http://www.treexas.com/");
+		assertEquals("http://www.treexas.com/",driver.getCurrentUrl());
 	}
 	@Test 
 	public void plantPage() {
 		driver.get("http://www.treexas.com/plant_list/");
-		int size = driver.findElements(By.xpath("//a")).size();
+		try {
+			TimeUnit.SECONDS.sleep(10);
+		} catch (InterruptedException e2) {
+			e2.printStackTrace();
+		}
+		int size = driver.findElements(By.xpath("/html/body/div[2]/div")).size();
+		assertEquals(806,size);
 		for(int i = 0;i<size;i++) {
-			WebElement e = driver.findElements(By.xpath("//a")).get(i);
-			String href = e.getAttribute("href");
+			WebElement e = driver.findElements(By.xpath("/html/body/div[2]/div")).get(i);
+			String href = e.findElement(By.cssSelector("a")).getAttribute("href");
 			e.click();
+			List<WebElement> e1 = new ArrayList<WebElement>();
+			e1=driver.findElements(By.xpath("//*[@id=\"plant_fields\"]/div/font[1]"));
 			assertEquals(driver.getCurrentUrl(),href);
+			assertNotEquals(0,e1.size());
 			driver.navigate().back();
 		}
 	}
 	@Test
 	public void ecoregionsPage() {
 		driver.get("http://www.treexas.com/eco_list/");
-		int size = driver.findElements(By.xpath("//a")).size();
+		try {
+			TimeUnit.SECONDS.sleep(10);
+		} catch (InterruptedException e2) {
+			e2.printStackTrace();
+		}
+		int size = driver.findElements(By.xpath("/html/body/div/div[2]/div[2]/map/area")).size();
+		assertEquals(15,size);
 		for(int i = 0;i<size;i++) {
-			WebElement e = driver.findElements(By.xpath("//a")).get(i);
+			WebElement e = driver.findElements(By.xpath("/html/body/div/div[2]/div[2]/map/area")).get(i);
 			String href = e.getAttribute("href");
-			e.click();
-			assertEquals(driver.getCurrentUrl(),href);
+			driver.get(href);
+			List<WebElement> e1 = new ArrayList<WebElement>();
+			e1=driver.findElements(By.xpath("/html/body/div/div[2]/div/h1"));
+			assertNotEquals(0,e1.size());
+			assertEquals(href,driver.getCurrentUrl());
 			driver.navigate().back();
 		}
 		
@@ -69,14 +95,34 @@ public class SeleniumTestCases {
 	@Test
 	public void stateparksPage() {
 		driver.get("http://www.treexas.com/park_list/");
-		int size = driver.findElements(By.xpath("//a")).size();
+		try {
+			TimeUnit.SECONDS.sleep(10);
+		} catch (InterruptedException e2) {
+			e2.printStackTrace();
+		}
+		int size = driver.findElements(By.xpath("/html/body/div/div[3]/div")).size();
 		for(int i = 0;i<size;i++) {
-			WebElement e = driver.findElements(By.xpath("//a")).get(i);
-			String href = e.getAttribute("href");
+			WebElement e = driver.findElements(By.xpath("/html/body/div/div[3]/div")).get(i);
+			String href = e.findElement(By.cssSelector("a")).getAttribute("href");
 			e.click();
-			assertEquals(driver.getCurrentUrl(),href);
+			List<WebElement> e1 = new ArrayList<WebElement>();
+			e1=driver.findElements(By.xpath("/html/body/div[1]/div[2]/div/h1"));
+			assertNotEquals(0,e1.size());
+			assertEquals(href,driver.getCurrentUrl());
 			driver.navigate().back();
 		}
+	}
+	@Test
+	public void search() {
+		driver.get("http://www.treexas.com/");
+		WebElement in=driver.findElement(By.xpath("/html/body/div/form/div/input"));
+		WebElement but = driver.findElement(By.xpath("/html/body/div/form/div/div/button"));
+		in.sendKeys("ABELIA, CONFETTI");
+		but.click();
+		List<WebElement> e1 = new ArrayList<WebElement>();
+		e1 = driver.findElements(By.xpath("/html/body/div[2]/div/a"));
+		assertNotEquals(0,e1.size());
+		assertEquals("http://www.treexas.com/plant_profile/?id=0",e1.get(0).getAttribute("href"));
 	}
 	@AfterClass
 	public static void tearDown() {
