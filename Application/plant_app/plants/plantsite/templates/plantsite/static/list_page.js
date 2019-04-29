b04ba9d2ec134e7234df15889bb773456c2f292f
fei <feihe@utexas.edu>
2019-04-29 00:32:12 -0500

    refactor list page css file

---
+++ Application/plant_app/plants/plantsite/templates/plantsite/static/list_page.js
@@ -0,0 +1,50 @@
+
+
+
+
+
+
+// Eco_List page
+// When the user clicks on <div>, open the popup
+function transpecosfunc() {
+	document.getElementById("map").src = "{% static "ecoregions_transpecos.png" %}";
+
+}
+function edwardsfunc() {
+	document.getElementById("map").src = "{% static "ecoregions_edwards.png" %}";
+}
+function crosstimbersfunc() {
+	document.getElementById("map").src = "{% static "ecoregions_crosstimbers.png" %}";
+
+}
+function southplainsfunc() {
+	document.getElementById("map").src = "{% static "ecoregions_southplains.png" %}";
+
+}
+function postoakfunc() {
+	document.getElementById("map").src = "{% static "ecoregions_postoak.png" %}";
+
+}
+function pineywoodsfunc() {
+	document.getElementById("map").src = "{% static "ecoregions_pineywoods.png" %}";
+
+}
+function blacklandprairiefunc() {
+	document.getElementById("map").src = "{% static "ecoregions_blackland.png" %}";
+
+}
+function coastfunc() {
+	document.getElementById("map").src = "{% static "ecoregions_marshes.png" %}";
+
+}
+function highplainsfunc() {
+	document.getElementById("map").src = "{% static "ecoregions_highplains.png" %}";
+
+}
+function rollingplainsfunc() {
+	document.getElementById("map").src = "{% static "ecoregions_rollingplains.png" %}";
+
+}
+function leave() {
+	document.getElementById("map").src = "{% static "ecoregions2.png" %}";
+}
